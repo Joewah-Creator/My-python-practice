da908d525adf9f7fd06e30c0ca40a86c44ca2447
josphat wahome <wahomejosphatt27@gmail.com>
2025-09-17 13:34:58 +0300

    Add code on practice.py

---
+++ python_practice/practice.py
@@ -0,0 +1,18 @@
+# define days
+day = 0
+if day == 1:
+    print("its monday")
+elif day == 2:
+    print("welcome tuesday")
+elif day == 3:
+    print("lock in wednesday")
+elif day == 4:
+    print("its throw back thursday")
+elif day == 5:
+    print("end week friday")
+elif day == 6:
+    print("wedding saturday")
+elif day == 7:
+    print("church day sunday")
+else: # print the end 
+    print("done")
